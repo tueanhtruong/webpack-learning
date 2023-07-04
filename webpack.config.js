69e3b7ff986fbd0e455f877ceb70f7859cfbbba0
TUE_TRUONG <tue_truong@datahouse.com>
2023-07-04 13:38:57 +0700

    feat: first setup webpack playground

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// Webpack 5
+// const nodeExternals = require("webpack-node-externals");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  //   externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
+  //   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      title: "Webpack Example App",
+      header: "Webpack Example Title",
+      metaDesc: "Webpack Example Description",
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+  module: {
+    rules: [{ test: /\.handlebars$/, loader: "handlebars-loader" }],
+  },
+  resolve: {
+    alias: {
+      handlebars: "handlebars/dist/handlebars.js",
+    },
+  },
+  output: {
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    port: 8080,
+    hot: true,
+  },
+};
