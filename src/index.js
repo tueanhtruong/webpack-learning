fe3fd8de27d0cb97594a593679d9e3b51814c9ff
TUE_TRUONG <tue_truong@datahouse.com>
2023-07-13 17:35:19 +0700

    feat: review p 85

--- src/index.js
+++ src/index.js
@@ -1,106 +1,134 @@
 import Handlebars from "handlebars";
 
 const template1 = `<div class="container">
-        <img src="{{imgUrl}}" style="width: 200px; margin-bottom: 48px" />
-        <div class="view-item">
-          <div class="view-item__label">Register ID</div>
-          <div class="view-item__value">{{registerId}}</div>
-        </div>
-        <div class="view-item">
-          <div class="view-item__label">Staff</div>
-          <div class="view-item__value">{{staff}}</div>
-        </div>
-        <div class="view-item">
-          <div class="view-item__label">Transaction Date</div>
-          <div class="view-item__value">{{transationDate}}</div>
-        </div>
-        <div class="view-item">
-          <div class="view-item__label">Transaction Number</div>
-          <div class="view-item__value">{{transationnumber}}</div>
-        </div>
-        <div class="view-item" style="margin-bottom: 40px">
-          <div class="view-item__label">Payer</div>
-          <div class="view-item__value">{{payer}}</div>
-        </div>
-        <div class="sub-label" style="margin-bottom: 16px">Description</div>
-        <table style="margin-bottom: 16px">
-          <thead>
-            <tr>
-              <th>Product Name</th>
-              <th>EPA Registration Number</th>
-              <th>Type</th>
-              <th>Company</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {{#each tableData}}
-            <tr>
-              <td>{{this.name}}</td>
-              <td>{{this.epaNumber}}</td>
-              <td>{{this.type}}</td>
-              <td>{{this.company}}</td>
-              <td>{{this.price}}</td>
-            </tr>
-            {{/each}}
-          </tbody>
-        </table>
-        <div class="view-item">
-          <div class="view-item__label">Amount Due</div>
-          <div class="view-item__value">{{amountDue}}</div>
-        </div>
-        <div class="view-item" style="margin-bottom: 40px">
-          <div class="view-item__label">Amount Paid</div>
-          <div class="view-item__value">{{amountPaid}}</div>
-        </div>
-        <div class="sub-label">Check</div>
-        <div class="view-item">
-          <div class="view-item__label">Check Date</div>
-          <div class="view-item__value">{{checkDate}}</div>
-        </div>
-        <div class="view-item">
-          <div class="view-item__label">Check #{{checkId}}</div>
-          <div class="view-item__value">{{payment}}</div>
-        </div>
-      </div>`;
+<!-- header -->
+<div class="header">
+  <div class="header--info">
+    <h2 class="text-primary text-font-primary mb-1">Credit Memorandum</h2>
+    <h4 class="text-primary text-font-sub">
+      CM No. CM-22-11 <br />
+      Date: {{date}}
+    </h4>
+  </div>
+  <div class="header--logo">
+    <img
+      src={{imgUrl}}
+      style="width: 172.999px; height: 52.621px"
+    />
+  </div>
+</div>
+<!-- General Information -->
+<div class="info">
+  <div class="text-font-title text-primary-200">General Information</div>
+  <div class="view-item">
+    <div class="view-item__label">Company Name </div>
+    <div class="view-item__value">{{companyName}}</div>
+  </div>
+  <div class="view-item">
+    <div class="view-item__label">Agent Name</div>
+    <div class="view-item__value">{{agentName}}</div>
+  </div>
+  <div class="view-item">
+    <div class="view-item__label">Agent Address</div>
+    <div class="view-item__value">{{agentAddress}}</div>
+  </div>
+</div>
+
+<!-- Refund information -->
+<div class="info">
+  <div class="text-font-title text-primary-200">Refund Information</div>
+
+  <table style="margin-bottom: 16px">
+    <thead>
+      <tr>
+        <th>Description Refund Amount</th>
+        <th>Refund Amount</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- {{#each tableData}} -->
+      <tr>
+        <td>
+          <p style="margin: 8px 0">REFUND DUE</p>
+          <br />
+          <br />
+          <p style="margin: 8px 0">
+            Refund fees, request to withdraw the following applications:
+          </p>
+          <p style="white-space: pre-line; line-height: 2">
+            {{this.applicationName}}
+          </p>
+          <br />
+          <br />
+          <p style="margin: 8px 0">Account: {{this.accountNumber}}</p>
+
+          <br />
+          <br />
+
+          <p style="margin: 8px 0">Deposit Slip #: {{this.depositSlip}}</p>
+          <p style="margin: 8px 0">Deposit Slip Date: {{this.depositSlipDate}}</p>
+          <br />
+          <br />
+          <p style="margin: 8px 0; font-weight: 600">Check #: {{checkNumber}}</p>
+          <p style="margin: 8px 0; font-weight: 600">Check Date: {{checkDate}}</p>
+          <p style="margin: 8px 0; font-weight: 600">Mailed: {{mailed}}</p>
+          <br />
+          <br />
+        </td>
+        <td>{{this.price}}</td>
+      </tr>
+      <!-- {{/each}} -->
+    </tbody>
+  </table>
+</div>
+
+<div class="footer info">
+  <div class="footer--left">
+    <h4 class="text-gray-700">P-85</h4>
+    <span class="text-gray-500">07/12/2023</span>
+  </div>
+  <div class="footer--right">
+    <h4 class="text-gray-700">Pesticide Branch</h4>
+    <p class="text-gray-500">{{mailingAddress}}</p>
+    <p class="text-gray-500">{{state}}, {{city}}, {{zipCode}}</p>
+  </div>
+</div>
+</div>`;
 
 const template1Func = Handlebars.compile(template1);
 const tableData = [
   {
     name: "Kroger Pet Pride Flea Spray 45-68 lbs",
-    epaNumber: "1234567890",
-    type: "Chemical",
+    epaNumber: "1234567wqeqwe890",
+    applicationName: [
+      "TrueLiving Cleaner with Bleach, EPA Reg. No. 70271-15-96374, 12345",
+      "TrueLiving Cleaner with Bleach, EPA Reg. No. 70271-15-96374, 12345",
+      "TrueLiving Cleaner with Bleach, EPA Reg. No. 70271-15-96374, 12345",
+      "TrueLiving Cleaner with Bleach, EPA Reg. No. 70271-15-96374, 12345",
+      "TrueLiving Cleaner with Bleach, EPA Reg. No. 70271-15-96374, 12345",
+    ].join("\n"),
+    accountNumber: "0964",
+    depositSlip: "12345",
+    depositSlipDate: " 01/26/2022",
     company: "Datahouse Asia",
     price: "$930",
-  },
-  {
-    name: "Kroger Pet Pride Flea Spray 45-68 lbs",
-    epaNumber: "1234567890",
-    type: "Device",
-    company: "Datahouse Asia",
-    price: "$930",
-  },
-  {
-    name: "Kroger Pet Pride Flea Spray 45-68 lbs",
-    epaNumber: "1234567890",
-    type: "Chemical",
-    company: "Datahouse Asia",
-    price: "$930",
+    checkNumber: "1234474",
+    checkDate: "30/08/2022",
+    mailed: "dung_do@datahouse.com",
   },
 ];
 const temp1obj1 = {
   imgUrl:
     "https://pesticides-web-storage-develop.s3.us-west-2.amazonaws.com/public/logo-splash.svg",
-  registerId: "REG03",
-  staff: "John Doe",
-  transationDate: "5/9/2023 1:23:59 PM",
-  transationnumber: "9142",
-  payer: "Pyxis Regulatory Consulting, Inc.",
-  amountDue: "2190 $",
-  amountPaid: "2190 $",
-  checkDate: "5/9/2023 1:23:59 PM",
-  payment: "2190 $",
+  date: "5/9/2023 1:23:59 PM",
   tableData,
+  companyName: "Data House",
+  agentName: "Dung Do",
+  agentAddress: "Da Nang",
+  mailingAddress: "1428 South King Street",
+  state: "1428 South King Street",
+  city: "Hawaii ",
+  zipCode: "96814",
 };
 
 const temp1FromObj1 = template1Func(temp1obj1);
